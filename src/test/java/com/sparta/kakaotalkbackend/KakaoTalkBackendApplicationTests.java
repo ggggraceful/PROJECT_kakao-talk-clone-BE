785162e5844ecf4d9cf361673cd30194260731a7
yuns8708 <yuns8708@gmail.com>
2022-10-28 13:44:15 +0900

    project start

---
+++ src/test/java/com/sparta/kakaotalkbackend/KakaoTalkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.kakaotalkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoTalkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
