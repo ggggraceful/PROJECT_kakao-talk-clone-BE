f6ca665fe4fad3bbe667c9f82fab785e9b28b04c
yuns8708 <yuns8708@gmail.com>
2022-10-30 22:32:28 +0900

    [Add] Add Friend + Get Friend List + Get Friend profile

--- src/main/java/com/sparta/kakaotalkbackend/domain/friend/Friend.java
+++ src/main/java/com/sparta/kakaotalkbackend/domain/friend/Friend.java
@@ -1,4 +1,41 @@
 package com.sparta.kakaotalkbackend.domain.friend;
 
+import com.sparta.kakaotalkbackend.domain.member.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Friend {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "friend_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    // myUsername = 현재 로그인된 사람의 username / username = 친구의(상대방의) username
+    @Column
+    private String myUsername;
+
+    @Column
+    private String username;
+
+    @Column
+    private String nickname;
+
+    @Column
+    private String image;
+
+    @Column
+    private String status;
 }
---
+++ src/main/java/com/sparta/kakaotalkbackend/domain/friend/FriendResponseDto.java
@@ -0,0 +1,14 @@
+package com.sparta.kakaotalkbackend.domain.friend;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FriendResponseDto {
+        private String friendName;
+//    private String myName;
+//    private String friendName;
+}
